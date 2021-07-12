feb612cf48f96469adbff802f6d3f1ca017079df
Mayk <kmi8652@gmail.com>
2021-07-12 11:31:21 +0300

    v21

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -38,8 +38,8 @@ resource "aws_security_group" "group1" {
 
   ingress {
     description = "app from anywhere"
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 0
+    to_port     = 0
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
